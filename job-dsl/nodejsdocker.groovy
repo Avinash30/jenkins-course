e4647e749bad3b68c625e78219ffb1309a6b8282
Avinash30 <gv.avinash3@gmail.com>
2019-10-29 10:07:13 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -2,7 +2,7 @@ job('NodeJS Docker example') {
     scm {
         git('git://github.com/Avinash30/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('gv.avinash3@gmail.com')
         }
     }
     triggers {
